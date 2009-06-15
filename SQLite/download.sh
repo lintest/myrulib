f8154bbd51fc0d089166b365ab5300b26e77e0ed
Kandrashin Denis <mail@lintest.ru>
2009-06-15 15:10:59 +0400

    Test and fix lib.rus.ec bases download script

--- SQLite/download.sh
+++ SQLite/download.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 for t in libactions libavtoraliase libavtorname libavtor libblocked libbook.old libbook libdonations libfilename libgenrelist libgenre libjoinedbooks libpolka libseqname libseq libsrclang libtranslator;
 do
-wget http://rus.ec/sql/$t;
-zcat $t | mysql $DBNAME -u root lib;
+wget http://lib.rus.ec/sql/lib.$t.sql.gz;
+zcat lib.$t.sql.gz | mysql $DBNAME -u root lib;
 done
 
 
