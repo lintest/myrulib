187048561f868e2f244a870db351b371d49efa3e
Kandrashin Denis <mail@lintest.ru>
2009-08-20 12:19:35 +0400

    Change MySQL to SQLite download script

--- SQLite/download.sh
+++ SQLite/download.sh
@@ -4,10 +4,16 @@
 #
 #for t in libactions libavtoraliase libavtorname libavtor libblocked libbook.old libbook libdonations libfilename libgenrelist libgenre libjoinedbooks libpolka libseqname libseq libsrclang libtranslator;
 #
+
+rm *.sql.gz
 for t in libavtoraliase libavtorname libavtor libbook libfilename libgenrelist libgenre libjoinedbooks libpolka libseqname libseq libsrclang libtranslator;
 do
-wget http://lib.rus.ec/sql/lib.$t.sql.gz;
-zcat lib.$t.sql.gz | mysql $DBNAME -u root lib;
+    wget http://lib.rus.ec/sql/lib.$t.sql.gz;
+    zcat lib.$t.sql.gz | mysql $DBNAME -u root -D lib
 done
 
+rm MyRuLib.db
 php convert.php
+
+rm MyRuLib.db.zip
+zip MyRuLib.db.zip MyRuLib.db
