003f686f85564e57c1f67e8419198bc039dad0d5
Kandrashin Denis <mail@lintest.ru>
2009-10-01 11:24:47 +0400

    Small changes

--- MyRuLib/FbConst.cpp
+++ MyRuLib/FbConst.cpp
@@ -1,7 +1,7 @@
 #include "FbConst.h"
 
-const wxString strVersionInfo = _T("MyRuLib - version 0.08 (alpha)\n\nhttp://myrulib.lintest.ru\nmail@lintest.ru");
-const wxString strHomePage = _T("http://myrulib.lintest.ru");
+const wxString strVersionInfo = _T("MyRuLib - version 0.09 (alpha)\n\nhttp://myrulib.lintest.ru\nmail@lintest.ru");
+const wxString strHomePage = wxT("http://myrulib.lintest.ru");
 
 const wxString alphabetRu = _("АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЭЮЯ");
 const wxString alphabetEn = wxT("#ABCDEFGHIJKLMNOPQRSTUVWXWZ");
@@ -9,8 +9,8 @@ const wxString strAlphabet = alphabetRu + alphabetEn;
 const wxString strNobody = _("(без автора)");
 const wxString strRusJE = wxT("е");
 const wxString strRusJO = wxT("ё");
-const wxString strOtherSequence = wxT("(разное)");
-const wxString strBookNotFound = wxT("Не найден архив%s, содержащий файл%s.");
+const wxString strOtherSequence = _("(разное)");
+const wxString strBookNotFound = _("Не найден архив%s, содержащий файл%s.");
 
 const int ciNoAuthor = 0x7FFFFFFF;
 
