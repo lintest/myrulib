c0a6c04098c49b59a604ed41b37da48c9c5240cf
Kandrashin Denis <mail@lintest.ru>
2009-10-18 00:41:13 +0400

    Fix export menu item image

--- MyRuLib/FbFrameBaseMenu.cpp
+++ MyRuLib/FbFrameBaseMenu.cpp
@@ -9,7 +9,7 @@ FbFrameBaseMenu::FbFrameBaseMenu(): wxMenuBar()
 	menu->AppendImg(wxID_NEW,  _("Добавить файл\tCtrl+N"), wxART_NEW);
 	menu->AppendImg(wxID_OPEN, _("Добавить директорию\tCtrl+O"), wxART_FOLDER_OPEN);
 	menu->AppendSeparator();
-	menu->AppendImg(wxID_SAVE, _("Экспорт книг\tCtrl+S"), wxART_FOLDER_OPEN);
+	menu->AppendImg(wxID_SAVE, _("Экспорт книг\tCtrl+S"), wxART_FILE_SAVE);
 	menu->AppendImg(wxID_EXIT, _("Выход\tAlt-F4"), wxART_QUIT);
 	Append(menu, _("Файл"));
 
