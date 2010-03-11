d9f25b4669e614f73dc51b49643d79815b3d3538
Kandrashin Denis <mail@lintest.ru>
2010-03-11 17:44:52 +0300

    DataVew/FbTreeList.cpp

---
+++ DataView/FbTreeModel.cpp
@@ -0,0 +1,193 @@
+#include "FbTreeModel.h"
+#include "FbBookModel.h"
+
+// -----------------------------------------------------------------------------
+// class FbTreeModelData
+// -----------------------------------------------------------------------------
+
+#define BOOK_CASHE_SIZE 64
+
+FbTreeModelData::FbTreeModelData(wxSQLite3ResultSet &result)
+{
+	m_rowid = result.GetInt(0);
+	m_bookid = result.GetInt(1);
+	m_title = result.GetAsString(2);
+	m_filesize = result.GetInt(3);
+}
+
+FbTreeModelData::FbTreeModelData(const FbTreeModelData &data) :
+	m_rowid(data.m_rowid),
+	m_bookid(data.m_bookid),
+	m_title(data.m_title),
+	m_authors(data.m_authors),
+	m_filesize(data.m_filesize)
+{
+}
+
+wxString FbTreeModelData::GetAuthors(wxSQLite3Database &database)
+{
+	if (!m_authors.IsEmpty()) return m_authors;
+
+	wxString sql = wxT("SELECT full_name FROM authors WHERE id IN (SELECT id_author FROM books WHERE id=?) ORDER BY search_name");
+	wxSQLite3Statement stmt = database.PrepareStatement(sql);
+	stmt.Bind(1, (int)m_bookid);
+	wxSQLite3ResultSet result = stmt.ExecuteQuery();
+	while (result.NextRow()) {
+		if (!m_authors.IsEmpty()) m_authors += wxT(", ");
+		m_authors += result.GetString(0).Trim(true);
+	}
+	return m_authors;
+}
+
+wxString FbTreeModelData::Format(const int number)
+{
+	int hi = number / 1000;
+	int lo = number % 1000;
+	if (hi)
+		return Format(hi) + wxT(" ") + wxString::Format(wxT("%03d"), lo);
+	else
+		return wxString::Format(wxT("%d"), lo);
+}
+
+wxString FbTreeModelData::GetValue(unsigned int col)
+{
+	switch (col) {
+		case FbTreeModel::COL_ROWID:
+			return wxString::Format("%d", m_rowid);
+		case FbTreeModel::COL_BOOKID:
+			return wxString::Format("%d", m_bookid);
+		case FbTreeModel::COL_TITLE:
+			return m_title;
+		case FbTreeModel::COL_SIZE:
+			return Format(m_filesize);
+		default:
+			return wxEmptyString;
+	}
+}
+
+// -----------------------------------------------------------------------------
+// class FbTreeModelCashe
+// -----------------------------------------------------------------------------
+
+WX_DEFINE_OBJARRAY(FbTreeModelArray);
+
+FbTreeModelCashe::FbTreeModelCashe(const wxString &filename)
+	: m_rowid(0)
+{
+    m_database.Open(filename);
+}
+
+unsigned int FbTreeModelCashe::RowCount()
+{
+    wxSQLite3ResultSet result = m_database.ExecuteQuery(wxT("SELECT COUNT(id) FROM books"));
+    return result.NextRow() ? result.GetInt(0) : 0;
+}
+
+FbTreeModelData FbTreeModelCashe::FindRow(unsigned int rowid)
+{
+	if ( m_rowid <= rowid && rowid < m_rowid + GetCount() ) return operator[]( rowid - m_rowid );
+
+	m_rowid = rowid <= BOOK_CASHE_SIZE ? 1 : rowid - BOOK_CASHE_SIZE;
+
+	Empty();
+	wxString sql = wxT("SELECT rowid, id, title, file_size FROM books WHERE rowid>=? ORDER BY 1 LIMIT ?");
+	wxSQLite3Statement stmt = m_database.PrepareStatement(sql);
+	stmt.Bind(1, (wxLongLong)m_rowid);
+	stmt.Bind(2, BOOK_CASHE_SIZE * 2);
+	wxSQLite3ResultSet result = stmt.ExecuteQuery();
+
+	FbTreeModelData * found = NULL;
+	unsigned int id = rowid;
+	while (result.NextRow()) {
+		FbTreeModelData * data = new FbTreeModelData(result);
+		while ( id < data->Id() ) {
+			Add(new FbTreeModelData(id++));
+		}
+		if (id == rowid) found = data;
+		Add(data);
+		id++;
+	}
+
+	if (found) return *found;
+
+	return rowid;
+}
+
+wxString FbTreeModelCashe::GetValue(unsigned int row, unsigned int col)
+{
+	return FindRow(row + 1).GetValue(col);
+}
+
+bool FbTreeModelCashe::GetValue(wxVariant &variant, unsigned int row, unsigned int col)
+{
+	FbTreeModelData data = FindRow(row + 1);
+
+    switch ( col ) {
+		case FbTreeModel::COL_ROWID: {
+			variant = wxString::Format("%d", row + 1);
+		} break;
+		case FbTreeModel::COL_TITLE: {
+//			variant << FbTitleData( data.GetValue(col), m_checked.Index(row) != wxNOT_FOUND );
+		} break;
+		case FbTreeModel::COL_AUTHOR: {
+			variant = data.GetAuthors(m_database);
+		} break;
+		default: {
+			variant = data.GetValue(col);
+		}
+	}
+	return true;
+}
+
+bool FbTreeModelCashe::SetValue(const wxVariant &variant, unsigned int row, unsigned int col)
+{
+    if (col == FbTreeModel::COL_TITLE) {
+        FbTitleData data;
+        data << variant;
+//        if (data.m_checked) m_checked.Add(row);
+//        else m_checked.Remove(row);
+    }
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+// class FbTreeModel
+// -----------------------------------------------------------------------------
+
+FbTreeModel::FbTreeModel(const wxString &filename)
+{
+    m_datalist = new FbTreeModelCashe(filename);
+}
+
+FbTreeModel::~FbTreeModel()
+{
+    wxDELETE(m_datalist);
+}
+
+void FbTreeModel::GetValueByRow( wxVariant &variant, unsigned int row, unsigned int col ) const
+{
+   	m_datalist->GetValue(variant, row, col);
+}
+
+bool FbTreeModel::SetValueByRow( const wxVariant &variant, unsigned int row, unsigned int col )
+{
+   	return m_datalist->SetValue(variant, row, col);
+}
+
+bool FbTreeModel::GetAttrByRow( unsigned int row, unsigned int col, wxDataViewItemAttr &attr ) const
+{
+	return false;
+
+    switch ( col ) {
+        case COL_ROWID:
+            return false;
+
+        case COL_TITLE:
+            if ( !(row % 2) ) return false;
+            attr.SetColour(*wxLIGHT_GREY);
+            break;
+    }
+
+    return true;
+}
+
---
+++ DataView/FbTreeModel.h
@@ -0,0 +1,94 @@
+#ifndef __FBTREEMODEL_H__
+#define __FBTREEMODEL_H__
+
+#include <wx/wx.h>
+#include <wx/dataview.h>
+#include <wx/wxsqlite3.h>
+#include <wx/arrimpl.cpp>
+#include <wx/renderer.h>
+
+class FbTreeModelData
+{
+    public:
+        FbTreeModelData(unsigned int id = 0): m_rowid(id), m_bookid(0), m_filesize(0) {};
+        FbTreeModelData(wxSQLite3ResultSet &result);
+        FbTreeModelData(const FbTreeModelData &data);
+        wxString GetValue(unsigned int col);
+		wxString GetAuthors(wxSQLite3Database &database);
+        unsigned int Id() { return m_rowid; };
+    private:
+		wxString Format(const int number);
+    private:
+        int m_rowid;
+        int m_bookid;
+        wxString m_title;
+        wxString m_authors;
+        int m_filesize;
+};
+
+WX_DECLARE_OBJARRAY(FbTreeModelData, FbTreeModelArray);
+
+class FbTreeModelCashe: private FbTreeModelArray
+{
+	public:
+        FbTreeModelCashe(const wxString &filename);
+        wxString GetValue(unsigned int row, unsigned int col);
+		bool GetValue(wxVariant &variant, unsigned int row, unsigned int col);
+        bool SetValue(const wxVariant &variant, unsigned int row, unsigned int col);
+        unsigned int RowCount();
+	private:
+        FbTreeModelData FindRow(unsigned int rowid);
+	private:
+        wxSQLite3Database m_database;
+        unsigned int m_rowid;
+        wxArrayInt m_checked;
+};
+
+
+class FbTreeModel: public wxDataViewModel
+{
+    public:
+        enum COL
+        {
+            COL_ROWID,
+            COL_BOOKID,
+            COL_TITLE,
+            COL_SIZE,
+            COL_AUTHOR,
+            COL_GENRE,
+            COL_NUMBER,
+            COL_RATING,
+            COL_TYPE,
+            COL_LANG,
+            COL_MAX,
+        };
+
+        FbTreeModel(const wxString &filename);
+
+        virtual ~FbTreeModel();
+
+        // implementation of base class virtuals to define model
+
+        virtual unsigned int GetColumnCount() const
+        {
+            return COL_MAX;
+        }
+
+        virtual wxString GetColumnType( unsigned int col ) const
+        {
+            return wxT("string");
+        }
+
+        virtual void GetValue( wxVariant &variant, const wxDataViewItem &item, unsigned int col ) const;
+
+        virtual bool SetValue(const wxVariant &variant, const wxDataViewItem &item, unsigned int col);
+
+        virtual bool GetAttrByRow( unsigned int row, unsigned int col, wxDataViewItemAttr &attr ) const;
+
+        virtual bool SetValueByRow( const wxVariant &variant, unsigned int row, unsigned int col );
+
+    private:
+        FbTreeModelCashe * m_datalist;
+};
+
+#endif // __FBTREEMODEL_H__
