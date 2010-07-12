827325fd595a4c35091e6758456bb82dd5fb4716
Kandrashin Denis <mail@lintest.ru>
2010-07-12 16:34:48 +0400

    count books by genres

--- sources/MyRuLib/FbGenrTree.cpp
+++ sources/MyRuLib/FbGenrTree.cpp
@@ -1,4 +1,7 @@
 #include "FbGenrTree.h"
+#include "FbBookEvent.h"
+#include "FbDatabase.h"
+#include "FbConst.h"
 
 //-----------------------------------------------------------------------------
 //  FbGenrParentData
@@ -11,4 +14,52 @@ IMPLEMENT_CLASS(FbGenrParentData, FbParentData)
 //-----------------------------------------------------------------------------
 
 IMPLEMENT_CLASS(FbGenrChildData, FbChildData)
+
+wxString FbGenrChildData::GetValue(FbModel & model, size_t col) const
+{
+	switch (col) {
+		case 0: 
+			return m_name;
+		case 1: 
+			return m_count ? Format(m_count) : wxString();
+		default:
+			return wxEmptyString ;
+	}
+}
+
+//-----------------------------------------------------------------------------
+//  FbGenrListData
+//-----------------------------------------------------------------------------
+
+IMPLEMENT_CLASS(FbGenrListData, FbModelData)
+
+FbGenrListData::FbGenrListData(wxSQLite3ResultSet &result)
+	: m_code(result.GetString(0)), m_count(result.GetInt(1)) 
+{
+}
+
+//-----------------------------------------------------------------------------
+//  FbGenrChildData
+//-----------------------------------------------------------------------------
+
+void * FbGenrListThread::Entry()
+{
+	FbCommonDatabase database;
+
+	wxString sql = wxT("SELECT id_genre, COUNT(DISTINCT id_book) FROM genres GROUP BY id_genre");
+	wxSQLite3ResultSet result = database.ExecuteQuery(sql);
+
+	if (IsClosed()) return NULL;
+
+	FbModel * model = new FbListStore;
+	while (result.NextRow()) model->Append(new FbGenrListData(result));
+
+	if (IsClosed()) {
+		delete model;
+	} else {
+		FbModelEvent(ID_MODEL_CREATE, model).Post(m_frame);
+	}
+
+	return NULL;
+}
 
--- sources/MyRuLib/FbGenrTree.h
+++ sources/MyRuLib/FbGenrTree.h
@@ -2,6 +2,20 @@
 #define __FBGENRTREE_H__
 
 #include "FbTreeModel.h"
+#include "FbThread.h"
+
+class wxSQLite3ResultSet;
+
+class FbGenrListData: public FbModelData
+{
+	public:
+		FbGenrListData(wxSQLite3ResultSet & result);
+	private:
+		wxString m_code;
+		int m_count;
+		friend class FbGenrChildData;
+		DECLARE_CLASS(FbGenrListData);
+};
 
 class FbGenrParentData: public FbParentData
 {
@@ -19,16 +33,33 @@ class FbGenrChildData: public FbChildData
 {
 	public:
 		FbGenrChildData(FbModel & model, FbParentData * parent, const wxString &code, const wxString &name)
-			: FbChildData(model, parent), m_code(code), m_name(name) {}
-		virtual wxString GetValue(FbModel & model, size_t col) const
-			{ return col ? (wxString)wxEmptyString : m_name; }
+			: FbChildData(model, parent), m_code(code), m_name(name), m_count(0) {}
 		wxString GetCode() const
 			{ return m_code; }
+		void SetCount(int count)
+			{ m_count = count; }
+		bool operator==(const FbGenrListData & data) const
+			{ return m_code == data.m_code; }
+		FbGenrChildData & operator=(const FbGenrListData & data)
+			{ if (!m_count) m_count = data.m_count; return *this; }
+		virtual wxString GetValue(FbModel & model, size_t col) const;
 		FbMasterInfo GetInfo() const;
 	private:
 		wxString m_code;
 		wxString m_name;
+		int m_count;
 		DECLARE_CLASS(FbGenrChildData);
+};
+
+class FbGenrListThread: public FbThread
+{
+	public:
+		FbGenrListThread(wxEvtHandler * frame)
+			: FbThread(wxTHREAD_JOINABLE), m_frame(frame) {}
+	protected:
+		virtual void * Entry();
+	private:
+		wxEvtHandler * m_frame;
 };
 
 #endif // __FBGENRTREE_H__
