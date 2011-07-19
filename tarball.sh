00b0a09dd8572d1ec85dad779d748652dcbfc772
Kandrashin Denis <mail@lintest.ru>
2011-07-19 23:49:50 +0400

    Script: tarball.sh

---
+++ tarball.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+VERSION=`cat configure | grep PACKAGE_VERSION= | sed "s/.*=//" | sed "s/'//g"`  
+echo Version: $VERSION;
+
+rm -rf tarball
+mkdir tarball
+cd tarball
+
+mkdir myrulib-$VERSION
+cd myrulib-$VERSION
+
+cp -f ../../AUTHORS          .
+cp -f ../../INSTALL          .
+cp -f ../../LICENSE          .
+cp -f ../../README           .
+cp -f ../../ChangeLog        .
+cp -f ../../aclocal.m4       .
+cp -f ../../autoconf_inc.m4  .
+cp -f ../../config.guess     .
+cp -f ../../config.sub       .
+cp -f ../../configure        .
+cp -f ../../configure.in     .
+cp -f ../../install-sh       .
+cp -f ../../Makefile.in      .
+cp -f ../../myrulib.bkl      .
+cp -rf ../../3rdparty        .
+
+mkdir sources
+
+cp -rf ../../sources/CREngine    sources
+cp -rf ../../sources/MyRuLib     sources
+cp -rf ../../sources/WxSQLite3   sources
+cp -rf ../../sources/ZipScaner   sources
+cp -f  ../../sources/config.h.in sources
+
+cd ..
+
+tar -cvjf myrulib-$VERSION.tar.bz2 myrulib-$VERSION
+cp myrulib-$VERSION.tar.bz2 myrulib_$VERSION.orig.tar.bz2
+cd ..
