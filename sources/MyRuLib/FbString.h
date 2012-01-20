625031b7a47694bb72e6732aa16d04147b8b7c0f
Kandrashin Denis <mail@lintest.ru>
2012-01-20 14:49:56 +0400

    Fix build error

--- sources/MyRuLib/FbString.h
+++ sources/MyRuLib/FbString.h
@@ -6,13 +6,9 @@
 class FbString: public wxString  {
 	public: 
 		FbString(const char * psz): wxString(psz, wxConvUTF8) {}
-		wxString Translate() { return wxGetTranslation(*this); }
+		wxString Translate() const { return wxGetTranslation(*this); }
 };
 
-wxString FbTransl(const char * psz);
-
 #define fbT(str) (FbString(str))
-
-#define T(str) (FbTransl(str))
 
 #endif // __FBSTRING_H__
