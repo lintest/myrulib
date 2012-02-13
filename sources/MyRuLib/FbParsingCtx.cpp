af588f08ad210bb10c9ebcf685cbb4877ad3be8b
Kandrashin Denis <mail@lintest.ru>
2012-02-13 13:41:16 +0400

    Fix error string replacement

--- sources/MyRuLib/FbParsingCtx.cpp
+++ sources/MyRuLib/FbParsingCtx.cpp
@@ -546,8 +546,8 @@ static void Fb2TextReaderErrorFunc(void * arg, const char * msg, xmlParserSeveri
 {
 	int line = xmlTextReaderLocatorLineNumber(locator);
 	wxString err(msg, wxConvUTF8);
-	err.Replace(wxT('\n'), ' ');
-	err.Replace(wxT('\r'), ' ');
+	err.Replace(wxT('\n'), wxT(' '));
+	err.Replace(wxT('\r'), wxT(' '));
 	wxLogError(_("XML parsing error: '%s' at line %d"), err.c_str(), line);
 }
 
